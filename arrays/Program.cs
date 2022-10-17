afbcea9906310680eca5da959d981edd82e64cea
irekkkarimov <irekkkarimov@mail.ru>
2022-10-17 15:14:33 +0300

    2nd exercise

---
+++ arrays/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Random random = new Random();
+        int[] array = new int[20];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next();
+        }
+
+        int counter = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if ((i != 0) && (i != (array.Length - 1)))
+            {
+                if ((array[i] > array[i - 1]) && (array[i] > array[i + 1]))
+                {
+                    counter++;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            else if (((i == 0) && (array[i] > array[i + 1])) || ((i == (array.Length - 1)) && (array[i] > array[i - 1])))
+            {
+                counter++;
+            }
+        }
+
+        Console.WriteLine(counter);
+    }
+}
